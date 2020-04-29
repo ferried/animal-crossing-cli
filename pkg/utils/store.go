e12baeeba587a05b3e72cd9ca075804ce4f40068
ferried <harlancui@Outlook.com>
2020-04-29 16:54:33 +0800

    fix(filePath): change api to ~/.acc

--- pkg/utils/store.go
+++ pkg/utils/store.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"github.com/mitchellh/go-homedir"
 	"io/ioutil"
 )
 
 func Store(fileName string) []byte {
-	data, err := ioutil.ReadFile("./api/" + fileName)
+	dir, _ := homedir.Dir()
+	data, err := ioutil.ReadFile(dir + "/.acc/" + fileName)
 	if nil == err {
 		return data
 	}
